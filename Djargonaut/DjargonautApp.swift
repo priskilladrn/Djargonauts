67a56064c321b0ce64184c4fe71ed488703bd50a
je-von <jevon.levin@gmail.com>
2023-04-18 11:35:07 +0700

    init

---
+++ Djargonaut/DjargonautApp.swift
@@ -0,0 +1,17 @@
+//
+//  DjargonautApp.swift
+//  Djargonaut
+//
+//  Created by Jevon Levin on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DjargonautApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
