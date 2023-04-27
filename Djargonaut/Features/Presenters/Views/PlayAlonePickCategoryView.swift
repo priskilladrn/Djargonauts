8b6cb7b23f24728bcf0184b5e5ac8864e0bbe832
priskilladrn <priskilla.adriani@gmail.com>
2023-04-27 12:12:06 +0700

    benerin choose category

--- Djargonaut/Features/Presenters/Views/PlayAlonePickCategoryView.swift
+++ Djargonaut/Features/Presenters/Views/PlayAlonePickCategoryView.swift
@@ -13,174 +13,111 @@ struct PlayAlonePickCategoryView: View {
     @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
-        if colorScheme == .light {
-            LightView
-        } else {
-            DarkView
-        }
-    }
-}
-
-private var LightView: some View {
-    GeometryReader { geo in
-        ZStack {
-            Image("bg_light_mode")
-                .resizable()
-                .scaledToFill()
-                .frame(width: geo.size.width, height: geo.size.height)
-            
-            VStack {
-                Spacer()
-                HStack {
-                    Button {
+        GeometryReader { geo in
+            ZStack {
+                Image("background")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: .infinity, height: .infinity)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    HStack {
+                        Image(systemName: "arrow.left")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: geo.size.width * 0.05)
+                            .padding(.leading, geo.size.width * 0.05)
+                            .padding(.trailing, geo.size.width * 0.02)
+                        Text("Pick a Category")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .foregroundColor(AppColor.title)
+                        Spacer()
+                    }
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Button {
+                            
+                        } label: {
+                            VStack{
+                                Image("tech_category")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: geo.size.width * 0.3)
+                                Text("Technology")
+                                    .font(.title3)
+                                    .foregroundColor(AppColor.title)
+                            }
+                        }
+                        .padding(.trailing, geo.size.width * 0.1)
                         
-                    } label: {
-                        VStack{
-                            Image("tech_light")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: geo.size.width * 0.3)
-                            Text("Technology")
-                                .font(.title3)
-                                .foregroundColor(Color("Title"))
+                        Button {
+                            
+                        } label: {
+                            VStack{
+                                Image("design_category")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: geo.size.width * 0.3)
+                                Text("Design")
+                                    .font(.title3)
+                                    .foregroundColor(AppColor.title)
+                            }
                         }
                     }
-                    .padding(.trailing, geo.size.width * 0.1)
+                    .padding(.bottom, geo.size.height * 0.055)
+                    .padding(.top, geo.size.height * 0.08)
+                    
+                    HStack {
+                        Button {
+                            
+                        } label: {
+                            VStack{
+                                Image("game_category")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: geo.size.width * 0.3)
+                                Text("Gaming")
+                                    .font(.title3)
+                                    .foregroundColor(AppColor.title)
+                            }
+                        }
+                        .padding(.trailing, geo.size.width * 0.1)
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack{
+                                Image("accounting_category")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: geo.size.width * 0.3)
+                                Text("Accounting")
+                                    .font(.title3)
+                                    .foregroundColor(AppColor.title)
+                            }
+                        }
+                    }
+                    
+                    Spacer()
                     
                     Button {
                         
                     } label: {
-                        VStack{
-                            Image("design_light")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: geo.size.width * 0.3)
-                            Text("Design")
-                                .font(.title3)
-                                .foregroundColor(Color("Title"))
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill()
                         }
                     }
-                }
-                .padding(.bottom, geo.size.height * 0.055)
-                .padding(.top, geo.size.height * 0.08)
-                
-                HStack {
-                    Button {
-                        
-                    } label: {
-                        VStack{
-                            Image("game_light")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: geo.size.width * 0.3)
-                            Text("Gaming")
-                                .font(.title3)
-                                .foregroundColor(Color("Title"))
-                        }
-                    }
-                    .padding(.trailing, geo.size.width * 0.1)
+                    .frame(width: geo.size.width * 0.8, height: geo.size.height * 0.08)
                     
-                    Button {
-                        
-                    } label: {
-                        VStack{
-                            Image("accounting_light")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: geo.size.width * 0.3)
-                            Text("Accounting")
-                                .font(.title3)
-                                .foregroundColor(Color("Title"))
-                        }
-                    }
+                    Spacer()
                 }
-                
-                Spacer()
             }
         }
-        .ignoresSafeArea()
-    }
-}
-
-private var DarkView: some View {
-    GeometryReader { geo in
-        ZStack {
-            Image("bg_dark_opacity_50%")
-                .resizable()
-                .scaledToFill()
-                .frame(width: .infinity, height: .infinity)
-                .ignoresSafeArea()
-            
-            VStack {
-                Spacer()
-                HStack {
-                    Button {
-                        
-                    } label: {
-                        VStack{
-                            Image("tech_dark")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: geo.size.width * 0.3)
-                            Text("Technology")
-                                .font(.title3)
-                                .foregroundColor(.white)
-                        }
-                    }
-                    .padding(.trailing, geo.size.width * 0.1)
-                    
-                    Button {
-                        
-                    } label: {
-                        VStack{
-                            Image("design_dark")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: geo.size.width * 0.3)
-                            Text("Design")
-                                .font(.title3)
-                                .foregroundColor(.white)
-                        }
-                    }
-                }
-                .padding(.bottom, geo.size.height * 0.055)
-                .padding(.top, geo.size.height * 0.08)
-                
-                HStack {
-                    Button {
-                        
-                    } label: {
-                        VStack{
-                            Image("game_dark")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: geo.size.width * 0.3)
-                            Text("Gaming")
-                                .font(.title3)
-                                .foregroundColor(.white)
-                        }
-                    }
-                    .padding(.trailing, geo.size.width * 0.1)
-                    
-                    Button {
-                        
-                    } label: {
-                        VStack{
-                            Image("accounting_dark")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: geo.size.width * 0.3)
-                            Text("Accounting")
-                                .font(.title3)
-                                .foregroundColor(.white)
-                        }
-                    }
-                }
-                
-                Spacer()
-            }
-        }
-        .ignoresSafeArea()
     }
 }
 
@@ -191,7 +128,7 @@ struct PlayAlonePickCategoryView_Previews: PreviewProvider {
             PlayAlonePickCategoryView()
                 .environmentObject(JargonListViewModel(context: viewContext))
                 .environment(\.colorScheme, .light)
-            
+
             PlayAlonePickCategoryView()
                 .environmentObject(JargonListViewModel(context: viewContext))
                 .environment(\.colorScheme, .dark)
